2253aef124eb4fc06f040049e9a71a04ce11f526
Rhkellz <67848704+Rhkellz@users.noreply.github.com>
2025-03-07 11:26:13 -0600

    Update RTfuncs.cpp
    commented

--- RTfuncs.cpp
+++ RTfuncs.cpp
@@ -28,45 +28,26 @@ int Rencoder = 0;
 int Lencoder = 0;
 int RencTurn = 0;
 int LencTurn = 0;
-float encK = 0.5;
-// This should be called in setup() to enable and calibrate the
-// gyro.  It uses the LCD, yellow LED, and button A.  While the LCD
-// is displaying "Gyro cal", you should be careful to hold the robot
-// still.
-void turnSensorSetup() {
+
+void turnSensorSetup() {//this function calibrates the gyro, do no touch robot while yellow LED is on
   Wire.begin();
   imu.init();
-
   imu.enableDefault();
-
-  // Set the gyro full scale to 1000 dps because the default
-  // value is too low, and leave the other settings the same.
   imu.writeReg(LSM6::CTRL2_G, 0b10001000);
 
-  // Turn on the yellow LED in case the LCD is not available.
   ledYellow(1);
-
-  // Delay to give the user time to remove their finger.
   delay(500);
-
-  // Calibrate the gyro.
   float total = 0;
-  for (uint16_t i = 0; i < 2000; i++) {
-    // Wait for new data to be available, then read it.
+  for (uint16_t i = 0; i < 2000; i++) {//gets average reading as the offset
     while(!imu.readReg(LSM6::STATUS_REG) & 0x08);
     imu.read();
-
-    // Add the Z axis reading to the total.
     total += imu.g.z;
   }
   ledYellow(0); 
-  Serial.println(offsetConst);
   gyroOffset = total/2000 + offsetConst;
-  //Serial.println(gyroOffset);
 }
 
-// This should be called to set the starting point for measuring
-// a turn.  After calling this, turnAngle will be 0.
+
 void turnSensorReset() {
   gyroLastUpdate = micros();
   encLastUpdate = micros();
@@ -82,27 +63,15 @@ void encoderReset() {
 }
 
 
-void turnSensorUpdate() {
+void turnSensorUpdate() {//PID controller for the gyro
   imu.readGyro();
   turnRate = imu.g.z - gyroOffset;
-  // Figure out how much time has passed since the last update (dt)
   uint16_t m = micros();
   uint16_t dt = m - gyroLastUpdate;
   gyroLastUpdate = m;
-  // Multiply dt by turnRate in order to get an estimation of how
-  // much the robot has turned since the last update.
-  // (angular change = angular velocity * time)
   int32_t d = (int32_t)turnRate * dt;
-
-  // The units of d are gyro digits times microseconds.  We need
-  // to convert those to the units of turnAngle, where 2^29 units
-  // represents 45 degrees.  The conversion from gyro digits to
-  // degrees per second (dps) is determined by the sensitivity of
-  // the gyro: 0.035 degrees per second per digit.
-  //
-  // (0.035 dps/digit) * (1/1000000 s/us) * (2^29/45 unit/degree)
-  // = 7340032/17578125 unit/(digit*us)
-  turnAngle += (int64_t)d * 7340032 / 17578125;
+  
+  turnAngle += (int64_t)d * 7340032 / 17578125;//unit stuff idk
   turnAngleInt += d * dt;
   turnSpeed = -(int32_t)turnAngle / (turnAngle1 / 12)
     - (turnRate / 100)- turnAngleInt/(turnAngle1*100);
@@ -111,7 +80,7 @@ void turnSensorUpdate() {
 
 }
  
-void encturnSensorUpdate() {
+void encturnSensorUpdate() {//PID controller for the encoders
   Rencoder = encoders.getCountsRight() + RencTurn;
   Lencoder = encoders.getCountsLeft() + LencTurn;
   float encError = Lencoder - Rencoder;//if positive, leaning right, if negative, leaning left
@@ -137,7 +106,7 @@ void pturnLeft() {
   turnTime += 0.74;
 }
 
-void moveDist(float cmVal, float totTime) {
+void moveDist(float cmVal, float totTime) {//trapezoidal motion profile, yes its a mess
   encoderReset();
   float tVel = 0;
   float encoderVal = cmVal * 65.3485659;
@@ -156,7 +125,6 @@ void moveDist(float cmVal, float totTime) {
     Serial.println("stage 1");
     while ((millis()-initT)/1000 < totTime/3) {
       if (count % 4 == 0) {
-        //Serial.println(encoders.getCountsRight() - lastd);
         dx = ((encoders.getCountsRight() - lastd) / 65.3485659)/100.0;
         dt = (millis()-lastt)/1000.0;
         lastd = encoders.getCountsRight();
@@ -170,10 +138,8 @@ void moveDist(float cmVal, float totTime) {
         tVel = accl * (millis()-initT)/1000;
         speed += diff * (kp+20);
         constrain(speed, 0, 300);
-        //Serial.println(diff);
         if (tVel < 0.03) {
           tVel = 0.03;
-          //Serial.println(vel);
         }
       }
       if (useGyro) {
@@ -189,7 +155,6 @@ void moveDist(float cmVal, float totTime) {
     Serial.println("stage 2");
     while ((millis()-initT)/1000 < 2*totTime/3) {
       if (count % 8 == 0) {
-        //Serial.println(encoders.getCountsRight() - lastd);
         dx = ((encoders.getCountsRight() - lastd) / 65.3485659)/100.0;
         dt = (millis()-lastt)/1000.0;
         lastd = encoders.getCountsRight();
@@ -203,8 +168,6 @@ void moveDist(float cmVal, float totTime) {
         tVel = totTime/3 * accl;
         speed += diff * kp;
         constrain(speed, 0, 300);
-        //Serial.println(diff);
-        //constrain(speed, 30, 300);
       }
       if (useGyro) {
         turnSensorUpdate();
@@ -232,21 +195,6 @@ void moveDist(float cmVal, float totTime) {
         diff = tVel - vel;
         speed += diff * 115;
         constrain(speed, 0, 300);
-        /*if ((tVel < 0.05) && (abs(encoders.getCountsRight() - encoderVal)/2 > 29) && (accl > 0.1)) {
-          Serial.println("alt move");
-          int endSpeed = 0;
-          if (encoders.getCountsRight() < encoderVal) {
-            endSpeed = abs(encoders.getCountsRight() - encoderVal) * abs((millis()-initT)/1000 - totTime) * 7.0;
-            Serial.println(endSpeed);
-          }
-          while (encoders.getCountsRight() < encoderVal) {
-            turnSensorUpdate();
-            motors.setSpeeds(endSpeed-turnSpeed, endSpeed+turnSpeed);
-          }
-        }
-        if (encoders.getCountsRight() > encoderVal) {
-          break;
-        }*/
       }
       if (useGyro) {
         turnSensorUpdate();
@@ -263,7 +211,7 @@ void moveDist(float cmVal, float totTime) {
   encoderReset();
 }
 
-void constMoveDist(float cmVal, float totTime) {
+void constMoveDist(float cmVal, float totTime) {//constant velocity profile
   encoderReset();
   float tVel = (cmVal / 100)/totTime;
   Serial.println(tVel);
@@ -331,7 +279,6 @@ void turnSafety() {
 }
 int encTurnConst = 726;
 void turnLeft() {
-  //encK = 2;
   if (useGyro) {
     gyroLastUpdate = micros();
     turnAngle = 3*turnAngle90;
@@ -340,11 +287,9 @@ void turnLeft() {
     RencTurn -= 729;
   }
   turnSafety();
-  //encK = 6;
 }
 
 void turnRight() {
-  //encK = 2;
   if (useGyro) {
     gyroLastUpdate = micros();
     turnAngle = turnAngle90;
@@ -353,10 +298,8 @@ void turnRight() {
     LencTurn -= 722;
   }
   turnSafety();
-  //encK = 6;
 }
 void turn180() {
-  //encK = 2;
   if (useGyro) {
     gyroLastUpdate = micros();
     turnAngle = turnAngle90*2;
@@ -365,7 +308,6 @@ void turn180() {
     LencTurn -= encTurnConst*2;
   }
   turnSafety();
-  //encK = 6;
 }
 
 void move() {
